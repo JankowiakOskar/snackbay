a121c7ef03ee1203daea4af494ed1bcb86e9279e
Oskar Jankowiak <oskar.jankowiak@landingi.com>
2022-05-12 23:27:21 +0200

    added navigation packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import { registerRootComponent } from 'expo';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
-export default function App() {
+export default const App = () =>  {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <View style={styles.container}>
+        <Text>Open up App.tsx to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </SafeAreaProvider>
   );
 }
 
