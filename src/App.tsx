67c1e699a84a90dac2eb9e1ece05f5bcfc8fedc7
Oskar Jankowiak <oskar.jankowiak@landingi.com>
2022-05-12 23:16:59 +0200

    removed husky and lint staged

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  const hello = 'elo';
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
